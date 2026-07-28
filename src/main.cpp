6c87bdd81d0f19120f2695b2354925f38590d7e7
YeonwooSung <neos960518@gmail.com>
2026-07-28 20:51:58 +0900

    feat: Add main.cpp for entrypoint

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <numeric>
+#include <fstream>
+#include <queue>
+
+#define JSON_USE_IMPLICIT_CONVERSIONS 0
+#include "json.hpp"
+
+using json = nlohmann::json;
+
+int main(int argc, char *argv[]) {
+    //
+}
